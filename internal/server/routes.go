5010e33e3a58057ec50217af5e35d6084fef55d7
Andrei Gorohov PC <anrew1002@gmail.com>
2024-07-12 15:27:11 +0800

    add CORS

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,8 +1,21 @@
 package server
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
 
 func (s *Server) configureRoutes() {
+	s.mux.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	s.mux.Route("/api/v1", func(r chi.Router) {
 		r.Get("/rooms", s.GetRoom())
 		r.Post("/rooms", s.CreateRoom())
