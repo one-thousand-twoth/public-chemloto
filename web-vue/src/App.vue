642c9cc9be6ed43f5b5a627c47d3e1147d7a8566
Andrei Gorohov PC <anrew1002@gmail.com>
2024-07-19 23:08:46 +0800

    websocket class

--- web-vue/src/App.vue
+++ web-vue/src/App.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 import Toaster from './components/Toaster.vue'
+import { WebsocketConnector } from './api/websocket/websocket';
+import { APISettings } from './api/config';
+import { provide } from 'vue';
+import { useUserStore } from './stores/useUserStore';
+
+const userStore = useUserStore()
+const connector = new WebsocketConnector(APISettings.baseURL,'')
+provide('connector', connector)
+if (userStore.UserCreds){
+    connector.token = userStore.UserCreds?.token
+}
+connector.Run()
+
 </script>
 <template>
     <router-view />
---
+++ web-vue/src/api/websocket/websocket.ts
@@ -0,0 +1,23 @@
+export class WebsocketConnector {
+    baseUrl: string;
+    token: string;
+    active: boolean;
+    connection!: WebSocket | null;
+
+    constructor(baseUrl: string, token: string = "") {
+        this.token = token;
+        this.baseUrl = baseUrl;
+        this.active = false;
+    }
+    Run() {
+        this.connection = new WebSocket(`ws://${this.baseUrl}/api/v1/ws?token=${this.token}`)
+        this.connection.onmessage = function (event) {
+            console.log(event);
+        }
+
+        this.connection.onopen = function (event) {
+            console.log(event)
+            console.log("Successfully connected to the echo websocket server...")
+        }
+    }
+}
