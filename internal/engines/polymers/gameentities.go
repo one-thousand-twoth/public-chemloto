a6fd554b963c6bd8f87dcdaee432e88871c0013d
Andrei Gorohov PC <anrew1002@gmail.com>
2024-11-26 19:50:40 +0800

    refactor(go): связанные с игрой сущности вынесены в отдельный файл

---
+++ internal/engines/polymers/gameentities.go
@@ -0,0 +1,59 @@
+package polymers
+
+import (
+	"github.com/anrew1002/Tournament-ChemLoto/internal/engines/models"
+	"github.com/anrew1002/Tournament-ChemLoto/internal/engines/models/enerr"
+	"github.com/samber/lo"
+)
+
+// Hand defines struct for RaisedHand
+type Hand struct {
+	Player    *Player
+	Field     string
+	Name      string
+	Structure map[string]int
+}
+
+// Game field with Scoring counter
+type Field struct {
+	Score int
+}
+
+// decrementScore substract by one field.Score and returns old value.
+//
+// Use it to assign Score to the Player who compose the Field correctly
+func (f *Field) decrementScore() int {
+	o := f.Score
+	f.Score -= 1
+	return o
+}
+
+// a Player represents Participant with Player roles with engine specific data.
+type Player struct {
+	models.Participant
+	RaisedHand bool
+	Bag        map[string]int
+	Score      int // Game score only for Players
+}
+
+// setScore will decrease Participant`s setScore with min value = -2
+func (p *Player) setScore(score int) {
+	p.Score += score
+	if p.Score < -2 {
+		p.Score = -2
+	}
+}
+func (p *Player) raiseHand() error {
+	return nil
+}
+
+// checkIfHasElements проверяет есть ли у игрока достаточно elements в его сумке
+func (p *Player) checkIfHasElements(elements map[string]int) error {
+	_, ok := lo.FindKeyBy(elements, func(k string, v int) bool {
+		return v < p.Bag[k]
+	})
+	if ok {
+		return enerr.E("Указаны элементы которых нет в таком количестве", enerr.GameLogic)
+	}
+	return nil
+}
