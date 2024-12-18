1108391cd7b2a65893ba76473c3f11c9c7c6fb3b
Andrei Gorohov PC <anrew1002@gmail.com>
2024-12-18 16:56:38 +0800

    fix(vue): z-index Toaster element

--- web/src/components/Toaster.vue
+++ web/src/components/Toaster.vue
@@ -1,7 +1,7 @@
 <template>
     <Teleport to="body">
         <div v-if="toastStore.toasts.length"
-            class="absolute flex flex-col gap-4 right-4 bottom-4 rounded-lg shadow-lg bg-white z-10">
+            class="absolute flex flex-col gap-4 right-4 bottom-4 rounded-lg shadow-lg bg-white z-50">
             <div v-for="(toast, idx) in toastStore.toasts" :key="idx" class="p-4 flex flex-row gap-2">
                 <ExclamationCircleIcon v-if="toast.status === 'error'" class="size-6 text-red-500" />
                 <InformationCircleIcon v-if="toast.status === 'info'" class="size-6 text-blue-500" />
