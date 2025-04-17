4cdd9d517aedc60358c6fdb2672aaf765af3feb6
Andrei Gorohov PC <anrew1002@gmail.com>
2025-04-17 19:30:26 +0800

    feat: обработка кастомных ошибок приложения

--- internal/server/helpers.go
+++ internal/server/helpers.go
@@ -1,15 +1,16 @@
 package server
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
-	// "github.com/go-playground/validator/v10"
+
+	"github.com/anrew1002/Tournament-ChemLoto/internal/common/enerr"
 )
 
 func encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) error {
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(v); err != nil {
@@ -18,17 +19,6 @@ func encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) erro
 	return nil
 }
 
-func GenerateRandomStringURLSafe(n int) (string, error) {
-	b, err := GenerateRandomBytes(n)
-	return base64.URLEncoding.EncodeToString(b), err
-}
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
+func encodeError(w http.ResponseWriter, log *slog.Logger, err error) {
+	enerr.HTTPErrorResponse(w, log, err)
 }
