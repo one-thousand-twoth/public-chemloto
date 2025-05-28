083f562b04d9d3f02b1a9715d7c527abec9daffd
Andrei Gorohov PC <anrew1002@gmail.com>
2025-05-28 12:22:07 +0800

    убрал старые handlers

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,197 +1,181 @@
 package server
 
-// func (s *Server) Status() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		encode(w, r, http.StatusOK, map[string]any{
-// 			"Users":       s.hub.Users,
-// 			"Channels":    s.hub.Channels,
-// 			"Rooms":       s.hub.Rooms,
-// 			"Connections": s.hub.Connections,
-// 		})
-// 	}
-// }
+import (
+	"context"
+	"log/slog"
+	"net/http"
 
-// func (s *Server) GetRooms() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		encode(w, r, http.StatusOK, s.hub.Rooms)
-// 	}
-// }
-// func (s *Server) GetUsers() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		encode(w, r, http.StatusOK, s.hub.Users)
-// 	}
-// }
-// func (s *Server) GetUser() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		token := chi.URLParam(r, "token")
-// 		if token == "" {
-// 			w.WriteHeader(http.StatusBadRequest)
-// 			return
-// 		}
-// 		clnt, ok := s.hub.Users.GetByToken(token)
-// 		if !ok {
-// 			encode(w, r, http.StatusNotFound, clnt)
-// 			return
-// 		}
-// 		encode(w, r, http.StatusOK, clnt)
-// 	}
-// }
-// func (s *Server) DeleteUser() http.HandlerFunc {
-// 	type Response struct {
-// 		Error []string `json:"error"`
-// 	}
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		const op = "server.handlers.PatchUser"
-// 		log := s.log.With(slog.String("op", op))
-// 		username := chi.URLParam(r, "username")
-// 		if username == "" {
-// 			w.WriteHeader(http.StatusBadRequest)
-// 			return
-// 		}
-// 		clnt, ok := s.hub.Users.Get(username)
-// 		if !ok {
-// 			encode(w, r, http.StatusNotFound, Response{Error: []string{"Возможно пользователь был удалён"}})
-// 			return
-// 		}
-// 		if clnt.Room != "" {
-// 			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Нельзя изменить пользователя находящегося в игре"}})
-// 			return
-// 		}
+	"github.com/anrew1002/Tournament-ChemLoto/internal/common"
+	"github.com/anrew1002/Tournament-ChemLoto/internal/entities"
+	"github.com/anrew1002/Tournament-ChemLoto/internal/sl"
+	"github.com/anrew1002/Tournament-ChemLoto/internal/usecase"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
 
-// 		s.hub.Users.Remove(clnt.Name)
+func (s *Server) Login2() http.HandlerFunc {
+	type Request struct {
+		Name string `json:"name" validate:"required,min=1,safeinput"`
+		Code string `json:"code,omitempty"`
+	}
+	type Response struct {
+		Token string      `json:"token"`
+		Role  common.Role `json:"role"`
+		Error []string    `json:"error"`
+	}
 
-// 		log.Info("Deleted user", "user", username)
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "server.handlers.Login"
+		log := s.log.With(slog.String("op", op))
+		var req Request
 
-// 		encode(w, r, http.StatusOK, Response{})
-// 	}
-// }
-// func (s *Server) PatchUser() http.HandlerFunc {
-// 	type Request struct {
-// 		Role common.Role
-// 	}
-// 	type Response struct {
-// 		Error []string `json:"error"`
-// 	}
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		const op = "server.handlers.PatchUser"
-// 		log := s.log.With(slog.String("op", op))
-// 		username := chi.URLParam(r, "username")
-// 		if username == "" {
-// 			w.WriteHeader(http.StatusBadRequest)
-// 			return
-// 		}
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode body")
+			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
+			return
+		}
+		log.Debug("request body decoded", slog.Any("request", req))
 
-// 		var req Request
-// 		err := render.DecodeJSON(r.Body, &req)
-// 		if err != nil {
-// 			log.Error("failed to decode body", sl.Err(err))
-// 			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
-// 			return
-// 		}
-// 		clnt, ok := s.hub.Users.Get(username)
-// 		if !ok {
-// 			encode(w, r, http.StatusNotFound, Response{Error: []string{"Возможно пользователь был удалён"}})
-// 			return
-// 		}
-// 		if clnt.Room != "" {
-// 			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Нельзя изменить пользователя в игре"}})
-// 			return
-// 		}
-// 		clnt.Role = req.Role
-// 		encode(w, r, http.StatusOK, clnt)
-// 	}
-// }
-// func (s *Server) CreateRoom() http.HandlerFunc {
-// 	type Request struct {
-// 		hub.CreateRoomRequest
-// 	}
-// 	type Response struct {
-// 		Rooms any      `json:"rooms"`
-// 		Error []string `json:"error"`
-// 	}
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		const op = "server.handlers.CreateRoom"
-// 		log := s.log.With(slog.String("op", op))
-// 		var req Request
-// 		err := render.DecodeJSON(r.Body, &req)
-// 		if err != nil {
-// 			log.Error("failed to decode body", sl.Err(err))
-// 			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
-// 			return
-// 		}
-// 		log.Debug("request body decoded", slog.Any("request", req))
+		request := usecase.LoginRequest{
+			Name: req.Name,
+			Code: req.Code,
+		}
+		user, err := s.usecases.Login(s.log, request, s.code)
+		if err != nil {
+			encodeError(w, log, err)
+			return
+		}
 
-// 		if err := s.hub.AddNewRoom(req.CreateRoomRequest); err != nil {
-// 			log.Error("failed to add room", sl.Err(err))
-// 			switch validateErr := err.(type) {
-// 			case validator.ValidationErrors:
-// 				encode(w, r, http.StatusBadRequest, Response{Error: appvalidation.ValidationError(validateErr)})
-// 				return
-// 			}
-// 			if enerr.KindIs(enerr.Exist, err) {
-// 				encode(w, r, http.StatusConflict, Response{Error: []string{"Комната уже существует"}})
-// 				return
-// 			}
-// 			encode(w, r, http.StatusConflict, Response{Error: []string{"Сервер не смог создать комнату"}})
-// 			return
-// 		}
-// 		s.log.Info("Room created", "name", req.Name, "time", req.Time)
-// 		// s.hub.SendMessageOverChannel("default", models.Message{Type: websocket.TextMessage, Body: []byte(req.Name)})
-// 		encode(w, r, http.StatusOK, Response{Rooms: s.hub.Rooms, Error: []string{}})
-// 	}
-// }
+		encode(w, r, http.StatusCreated, Response{Token: user.Token, Role: user.Role})
+	}
+}
 
-// func (s *Server) Login() http.HandlerFunc {
-// 	type Request struct {
-// 		Name string `json:"name" validate:"required,min=1,safeinput"`
-// 		Code string `json:"code,omitempty"`
-// 	}
-// 	type Response struct {
-// 		Token string      `json:"token"`
-// 		Role  common.Role `json:"role"`
-// 		Error []string    `json:"error"`
-// 	}
+func (s *Server) PatchUser() http.HandlerFunc {
+	type Request struct {
+		Role common.Role
+	}
+	type Response struct {
+		Error []string `json:"error"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "server.handlers.PatchUser"
+		log := s.log.With(slog.String("op", op))
+		username := chi.URLParam(r, "username")
+		if username == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		const op = "server.handlers.Login"
-// 		log := s.log.With(slog.String("op", op))
-// 		var req Request
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode body", sl.Err(err))
+			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
+			return
+		}
+		err = s.usecases.PatchUserRole(context.TODO(), usecase.PatchRequest{
+			Name: username,
+			Role: req.Role,
+		})
+		if err != nil {
+			log.Error("failed to PatchUserRole", sl.Err(err))
+			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
+		}
 
-// 		err := render.DecodeJSON(r.Body, &req)
-// 		if err != nil {
-// 			log.Error("failed to decode body")
-// 			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
-// 			return
-// 		}
-// 		log.Debug("request body decoded", slog.Any("request", req))
-// 		validate := appvalidation.Ins
-// 		if err := validate.Struct(req); err != nil {
-// 			validateErr := err.(validator.ValidationErrors)
-// 			encode(w, r, http.StatusBadRequest, Response{Error: appvalidation.ValidationError(validateErr)})
-// 			return
-// 		}
-// 		var role common.Role
-// 		role = common.Player_Role
-// 		if req.Code != "" {
-// 			if req.Code == s.code {
-// 				role = common.Admin_Role
-// 			} else {
-// 				encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный код администратора"}})
-// 				return
-// 			}
-// 		}
-// 		token, err := GenerateRandomStringURLSafe(32)
-// 		if err != nil {
-// 			encode(w, r, http.StatusInternalServerError, Response{Error: []string{"Ошибка сервера"}})
-// 			return
-// 		}
-// 		channels := []string{"default"}
-// 		user := hub.NewUser(req.Name, token, "", role, channels)
-// 		if err := s.hub.Users.Add(user); err != nil {
-// 			encode(w, r, http.StatusConflict, Response{Error: []string{"Пользователь с таким именем уже существует"}})
-// 			return
-// 		}
-// 		s.log.Info("user registred", "name", req.Name, "role", role)
-// 		encode(w, r, http.StatusCreated, Response{Token: token, Role: role})
-// 	}
-// }
+		encode(w, r, http.StatusOK, struct{}{})
+	}
+}
+
+func (s *Server) CreateRoom2() http.HandlerFunc {
+	type Request struct {
+		Name         string         `json:"name" validate:"required,min=1,safeinput"`
+		Type         string         `json:"type"`
+		EngineConfig map[string]any `json:"engineConfig"`
+	}
+	type Response struct {
+		Rooms any      `json:"rooms"`
+		Error []string `json:"error"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "server.handlers.CreateRoom"
+		log := s.log.With(slog.String("op", op))
+
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode body", sl.Err(err))
+			encode(w, r, http.StatusBadRequest, Response{Error: []string{"Неправильный формат запроса"}})
+			return
+		}
+		log.Debug("request body decoded", slog.Any("request", req))
+		createRoomParams := usecase.CreateRoomParams{
+			Name:         req.Name,
+			Type:         req.Type,
+			EngineConfig: req.EngineConfig,
+		}
+
+		_, err = s.usecases.CreateRoom(createRoomParams, s.log)
+		if err != nil {
+			encodeError(w, log, err)
+			return
+		}
+		encode(w, r, http.StatusOK, Response{Rooms: nil, Error: []string{}})
+	}
+}
+
+func (s *Server) GetRooms2() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		rooms, err := s.usecases.GetRooms(context.TODO())
+		if err != nil {
+			encode(w, r, http.StatusInternalServerError, struct{}{})
+			return
+		}
+		roomMap := make(map[string]*entities.Room, len(rooms))
+		for _, v := range rooms {
+			roomMap[v.Name] = v
+		}
+		encode(w, r, http.StatusOK, roomMap)
+	}
+}
+
+func (s *Server) GetUser2() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := chi.URLParam(r, "token")
+		if token == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		clnt, err := s.usecases.UserRepo.GetUserByApikey(token)
+		if err != nil {
+			encode(w, r, http.StatusNotFound, clnt)
+			return
+		}
+		encode(w, r, http.StatusOK, clnt)
+	}
+}
+
+func (s *Server) DeleteUser() http.HandlerFunc {
+	type Response struct {
+		Error []string `json:"error"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "server.handlers.PatchUser"
+		log := s.log.With(slog.String("op", op))
+		username := chi.URLParam(r, "username")
+		if username == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Error("User delete", "user", username)
+		err := s.usecases.DeleteUser(context.TODO(), username)
+		if err != nil {
+			encodeError(w, log, err)
+			return
+		}
+
+		log.Info("Deleted user", "user", username)
+		encode(w, r, http.StatusOK, Response{})
+	}
+}
